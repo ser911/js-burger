448366eab4a943a0c4acfa0345b66a74eee7d6ff
Sergio <sergio.marretta@libero.it>
2021-03-05 18:04:01 +0100

    js start

---
+++ js/myscript.js
@@ -0,0 +1,20 @@
+// definizione variabili
+var burgerName = document.getElementById("burger-name");
+var calculate = document.getElementById("calc");
+
+// aggiunta evento click button
+calculate.addEventListener("click",
+function(){
+  // controllo nome
+
+if (burgerName.value !== "") {
+
+
+}
+else {
+    alert("Inserisci un nome valido")
+}
+}
+
+
+);
